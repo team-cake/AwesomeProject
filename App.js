baa75f9221c824e76d1eb36dee4ea83193fd61e4
team-cake <eka.cheuk@gmail.com>
2020-08-21 15:40:56 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from 'react'
+import {
+	Text,
+	View,
+	ActivityIndicator,
+	Image,
+	Button,
+	Alert,
+	ScrollView,
+} from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const randomNum = useRef(Math.random()).current
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	return (
+		<View style={{ marginHorizontal: 40, marginVertical: 60 }}>
+			<Text style={{ fontWeight: 'bold', fontSize: 24, marginBottom: 30 }}>
+				Hello React Native
+			</Text>
+			<ScrollView>
+				<ActivityIndicator
+					size='large'
+					color='#c1262c'
+					style={{ marginBottom: 30 }}
+				/>
+				{[0, 1, 2, 3, 4].map((i) => {
+					return (
+						<Image
+							key={i}
+							source={{
+								uri: `https://picsum.photos/500/300?random=${randomNum + i}`,
+							}}
+							style={{ width: '100%', height: 160, marginBottom: 30 }}
+						/>
+					)
+				})}
+
+				<View
+					style={{
+						borderWidth: 2,
+						borderColor: 'black',
+						padding: 20,
+						marginBottom: 30,
+					}}
+				>
+					<Text>Hello again!</Text>
+				</View>
+				<Button
+					onPress={() => Alert.alert('Learning RN is so easy')}
+					title='Learn More'
+					color='#c1262c'
+				/>
+			</ScrollView>
+		</View>
+	)
+}
